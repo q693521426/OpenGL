014004c195331c358532674cc34620725a5dceea
Su <693521426@qq.com>
2016-12-26 17:49:34 +0800

    添加项目文件。

---
+++ myRPG/RPG.cpp
@@ -0,0 +1,16 @@
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GL includes
+#include "Shader.h"
+#include "Camera.h"
+
+// GLM Mathemtics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Other Libs
+#include <SOIL.h>
